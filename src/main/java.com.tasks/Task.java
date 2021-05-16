d4de45e8cd9d6b6f34b91f0774c1ee83f3b3ead1
andrei.hincu <a.hincu@octopod.ru>
2021-05-16 22:25:35 +0300

    Первая программа. [#188590]

---
+++ src/main/java.com.tasks/Task.java
@@ -0,0 +1,7 @@
+import com.tasks.BaseKt;
+
+public class Task {
+    public static void main(String[] args) {
+        BaseKt.main(new String[0]);
+    }
+}
---
+++ src/main/kotlin/com/tasks/Base.kt
@@ -0,0 +1,5 @@
+package com.tasks
+
+fun main(args: Array<String>) {
+    println("Hello world")
+}
