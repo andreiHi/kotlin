48d11aaef9cdac149d1617c30a6a62c50b0011a7
andrei.hincu <a.hincu@octopod.ru>
2021-05-16 23:14:20 +0300

    Тестирование через KotlinTest [#188841]

---
+++ src/test/kotlin/com/tasks/BaseKtTest.kt
@@ -0,0 +1,15 @@
+package com.tasks
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class CalcTest: StringSpec({
+    "1 + 1 = 2" {
+        add(1, 1) shouldBe 2
+    }
+
+    "1 - 1 = 0" {
+        subtract(1, 1) shouldBe 0
+    }
+
+})
